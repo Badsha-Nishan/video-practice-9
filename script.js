bfa8ca18a6a093257e445f14d33328845e4b4a9c
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-02 14:30:26 +0600

    initialized

---
+++ script.js
@@ -0,0 +1,25 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadData = fetch(url)
+  .then((res) => res.json())
+  .then((data) => loadPost(data));
+
+//   {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//   },
+
+function loadPost(data) {
+  const container = document.querySelector(".container");
+  const div = document.createElement("div");
+  div.innerHTML = "";
+  data.forEach((element) => {
+    const li = document.createElement("li");
+    li.innerText = element.title;
+    div.appendChild(li);
+  });
+  container.append(div);
+  return div;
+}
